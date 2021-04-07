c3a87ad02ac6ddff6584dee77dd5ca3dcc8fd73d
juanmahecha9 <juanmahecha.dc@gmail.com>
2021-04-07 17:37:21 -0500

    agregando estilos con react

--- primera_app/src/components/Task.js
+++ primera_app/src/components/Task.js
@@ -1,21 +1,31 @@
 /* Archivo o componente para ingresar a solo una tarea */
 
 import React, { Component } from "react";
+/* importar estilos */
+import "./Task.css";
 
 class Task extends Component {
   render() {
-      const {task} = this.props
+    const { task } = this.props;
     return (
-      <div>
-        {task.title} - 
-        {task.description} - 
-        {task.done} - 
-        {task.id}
+      <div className="tarea">
+        {task.title} -{task.description} -{task.done} -{task.id}
         <input type="checkbox" />
-        <button>x</button>
+        <button style={btnDelete}>x</button>
       </div>
     );
   }
 }
+
+/* para estilizar elementos */
+
+const btnDelete = {
+  fontSize: "18px",
+  color: "white",
+  background: "#000",
+  padding: "10px",
+  borderRadius: "50%",
+  cursor: "pointer",
+};
 
 export default Task;
